207d49e9d3adeafe4dbe90ccec97391b983db25d
Nolan <nolan330@gmail.com>
2015-03-26 03:06:24 -0500

    relatively trashdick

--- back-end/hyperplant.js
+++ back-end/hyperplant.js
@@ -4,8 +4,8 @@
   var port = 3701;
       connect = require('connect'),
       serve = require('serve-static'),
-      io = require('socket.io').listen(
-        connect().use(serve("../index.html")).listen(port)),
+      app = connect().use(serve(__dirname + "/../")).listen(port),
+      io = require('socket.io').listen(app),
 
       // grid
       initgrid = function(length, width) {
@@ -23,7 +23,8 @@
         }
         return grid;
       },
-      gridX = 20, gridY = 20,
+      //TODO: fix gridsize <-> display size mapping
+      gridX = 45, gridY = 55,
       grid = initgrid(gridX, gridY),
 
       // player metadata
@@ -45,31 +46,31 @@
           newLocations = [],
           winner = false;
 
-      for(var player in Object.keys(players)) {
+      for(var player in players) {
         // calculate next location
         switch(players[player].direction) {
           case 1 /*left*/:
-            next[x] = players[player].x - 1;
-            next[y] = players[player].y;
+            next.x = players[player].x - 1;
+            next.y = players[player].y;
             break;
           case 2 /*up*/:
-            next[x] = players[player].x;
-            next[y] = players[player].y - 1;
+            next.x = players[player].x;
+            next.y = players[player].y - 1;
             break;
           case 3 /*right*/:
-            next[x] = players[player].x + 1;
-            next[y] = players[player].y;
+            next.x = players[player].x + 1;
+            next.y = players[player].y;
             break;
           case 4 /*down*/:
-            next[x] = players[player].x;
-            next[y] = players[player].y + 1;
+            next.x = players[player].x;
+            next.y = players[player].y + 1;
             break;
         }
 
         // lol
         if(grid[next.x][next.y]) {
           players[player].alive = false;
-          player.emit('crash', { rekt_irl: true });
+          players[player].socket.emit('crash', { rekt_irl: true });
           --alivePlayers;
         }
         else {
@@ -81,37 +82,41 @@
           grid[next.x][next.y] = true;
 
           // track new locations
-          newLocations.push(next.x, next.y);
+          newLocations.push({ x: next.x,
+                              y: next.y });
         }
       }
 
-      //TODO: handle replaying/waitQueue
-      for(var player in Object.keys(players)) {
+      for(var player in players) {
         if(players[player].alive) {
-          if(alivePlayers <= 1)
-            player.emit('win');
+          if(alivePlayers <= 1) {
+            players[player].socket.emit('win');
+
+            // stop the interval to stop the "game loop"
+            // note: this will continue running the current interval
+            clearInterval(interval);
+            //TODO: reset the game, handle waitQueue
+          }
           else
-            //TODO: in client, socket.on('update', ...) should be update(data[1]. data[2])
-            // ie remove need for object
-            player.emit('update', { locationList: newLocations });
+            players[player].socket.emit('update', { newLocations: newLocations });
         }
         else {
           if(alivePlayers <= 1)
-            player.emit('lose');
+            players[player].socket.emit('lose');
         }
-        // stop the interval to stop the "game loop"
-        clearInterval(interval);
       }
-    }, 1500);
+    //TODO: game mode difficulties by changing the update value
+    }, 100);
   };
 
   io.sockets.on('connection', function(socket) {
     ++numPlayers;
     ++alivePlayers;
+    console.log("Joined. Total players: " + numPlayers);
 
     // TODO: do we need playerNumber?
     // track players and seed their loc/dir as they stream in
-    players[socket] = { playerNumber: numPlayers,
+    players[socket.id] = { playerNumber: numPlayers,
                         alive: true,
                         x: (numPlayers % 2 == 0) ?
                               Math.floor(1/4 * gridX) :
@@ -119,48 +124,54 @@
                        // calculate the closest power of 2 greater than the number of players
                         y: Math.floor((1/(Math.pow(Math.ceil(
                               Math.log(MAX_ALLOWED_PLAYERS + 1)/Math.log(2)), 2))) * gridY),
-                        direction: (numPlayers % 2 == 0) ? 3/*right*/: 1/*left*/ };
+                        direction: (numPlayers % 2 == 0) ? 3/*right*/: 1/*left*/,
+                        socket: socket };
 
     // seed the locations as players stream in
-    seedLocations.push({ x: players[socket].x,
-                         y: players[socket].y });
+    seedLocations.push({ x: players[socket.id].x,
+                         y: players[socket.id].y });
 
     // set their initial location as taken
-    grid[players[socket].x][players[socket].y] = true;
+    grid[players[socket.id].x][players[socket.id].y] = true;
 
     socket.emit('join');
 
     // TODO: change the "ready" event to query the players if they want to wait for more players or go
     // wait until an appropriate number of players have joined to initiate game
     if (numPlayers >= MIN_ALLOWED_PLAYERS && numPlayers <= MAX_ALLOWED_PLAYERS) {
-      for(var player in Object.keys(players)) {
-        player.emit('ready');
+      for(var player in players) {
+        console.log("checking if player ready...");
+        players[player].socket.emit('ready');
       }
     }
     // TODO: do something with waitQueue
     // add extra players to a waiting queue and notify them
     if (gameStarted || numPlayers > MAX_ALLOWED_PLAYERS) {
+      console.log("adding player " + players[socket.id].playerNumber + " to wait Queue");
       socket.emit('waitqueue');
       waitQueue.push(socket);
     }
 
     socket.on('playerready', function(data) {
       ++readyPlayers;
+      console.log("Total players ready: " + readyPlayers);
 
       // if everyone is ready, start the game
       if (readyPlayers == numPlayers) {
+        console.log("starting game...");
         gameStarted = true;
-        for(var player in Object.keys(players)) {
-          player.emit('start', { playerNumber: players[player].playerNumber,
+        for(var player in players) {
+          players[player].socket.emit('start',
+                               { playerNumber: players[player].playerNumber,
                                  initialDirection: players[player].direction,
-                                 initialLocation: seedLocations });
+                                 initialLocations: seedLocations });
         }
         startGame();
       }
     });
 
     socket.on('direction', function(data) {
-      players[socket].direction = data.direction;
+      players[socket.id].direction = data.direction;
     });
   });
 })();
--- tron.js
+++ tron.js
@@ -1,205 +1,206 @@
-
 var socket;
 window.onload = function() {
-            socket = io.connect(window.location.href);
+  socket = io.connect(window.location.href);
 
-    var Tron = (function() {
-            var Grid = {
-                init: function() {
-                    var margin = {top: 20, right: 10, bottom: 20, left: 20};
-                    this.margin = margin;
-                    this.width = this.size - margin.left - margin.right;
-                    this.height = this.size - margin.top - margin.bottom;
-                    this.svg = d3.select("#tron").append("svg")
-                    .attr("width", this.width + margin.left + margin.right)
-                    .attr("height", this.height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var Tron = (function() {
+    var Grid = {
+      init: function() {
+        var margin = {top: 20, right: 10, bottom: 20, left: 20};
+        this.margin = margin;
+        this.width = this.size - margin.left - margin.right;
+        this.height = this.size - margin.top - margin.bottom;
+        this.svg = d3.select("#tron").append("svg")
+        .attr("width", this.width + margin.left + margin.right)
+        .attr("height", this.height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                    var squareSize = this.size/this.gridSize;
-                    var squares = [];
-                    var normal = [];
+        var squareSize = this.size/this.gridSize;
+        var squares = [];
+        var normal = [];
 
-                    //1 for grid pixel size
-                    var count = 0;
-                    for(var i = 0; i < this.size; i+= (squareSize + 1), count += 1) {
-                        normal.push(count);
-                        squares.push(i + margin.left);
-                    }
-                    this.nS = normal.length;
-                    console.log(normal);
-
-                    this.x = d3.scale.quantize().domain(normal)
-                    .range(squares);
-
-                    this.y = d3.scale.quantize().domain(normal)
-                    .range(squares);
-                },
-                nSquares: function() {
-                    return this.nS || 0;
-                },
-                draw: function() {
-                    var svg = this.svg;
-                    var rect = svg.append("rect")
-                    .attr({
-                            x: this.margin.left,
-                            y: this.margin.top,
-                            width: this.width,
-                            height: this.height,
-                            fill: 'black'
-                        });
-                    var grid = svg.append("g").attr("class", "grid");
-                    grid.selectAll("line.horizontalGrid").data(this.y.range()).enter()
-                    .append("line")
-                    .attr(
-            {
-                "class":"horizontalGrid",
-                    "x1" : this.x(0),
-                    "x2" : this.x(this.size),
-                    "y1" : function(d){ return d;},
-                    "y2" : function(d){ return d;},
-                        "fill" : "none",
-                            "shape-rendering" : "crispEdges",
-                            "stroke" : "green",
-                            "stroke-width" : "1px"
-                            });
-
-
-                    grid.selectAll("line.verticalGrid").data(this.x.range()).enter()
-                    .append("line")
-                    .attr(
-            {
-                "class":"horizontalGrid",
-                    "y1" : this.y(0),
-                    "y2" : this.y(this.size),
-                    "x1" : function(d){ return d;},
-                    "x2" : function(d){ return d;},
-                        "fill" : "none",
-                            "shape-rendering" : "crispEdges",
-                            "stroke" : "green",
-                            "stroke-width" : "1px"
-                            });
-                },
-                update: function(player1, player2) {
-                    this.players[0].push(player1);
-                    this.players[1].push(player2);
-
-                    var squares = this.svg.append("g").attr("class", "grid-squares");
-                    var p1 = squares.selectAll("rect.square.player1").data(this.players[0]);
-                    var p2 = squares.selectAll("rect.square.player2").data(this.players[1]);
-                    var x = this.x,
-                    y = this.y,
-                    squareSize = (this.size/this.gridSize + 1);
-
-                    console.log(x);
-                    var update_player = function(n, player, color) {
-
-                        player.attr("class", "square");
-                        player.enter().append("rect")
-                        .attr({
-                                class: "square player" + n,
-                                    x: function(d) { return x(d.x); },
-                                    y: function(d) { return y(d.y); },
-                                    width: squareSize,
-                                    height: squareSize,
-                                    fill: color
-                                    });
-                    };
-
-                    update_player(1, p1, "green");
-                    update_player(2, p2, "white");
-                },
-                players: [[],[]],
-                size: 600,
-                gridSize: 50
-            };
-
-            Grid.init();
-            Grid.draw();
-
-            return {
-                update: function(player1, player2) {
-                    Grid.update(player1, player2);
-                },
-                    size: function() {
-                    return Grid.nSquares();
-                }
-            };
-        })();
-
-
-var cPosition = {'x' : 150, 'y' : 150};
-//1 - left, 2-up, 3-right, 4-down
-var cDirection = 2;
-var MAX_X = 1000;
-var MAX_Y = 1000;
-var stepSize = 50;
-
-var isAlive = false;
-var playerNumber = 0;
-
-$(document).keydown(function(e){
-        if(isAlive == true){
-            //left arrow
-            if (e.keyCode == 37) {
-                cDirection = e.keyCode - 36;
-                socket.emit("direction", {'direction' : cDirection});
-                return false;
-            }
-            //up arrow
-            if (e.keyCode == 38) {
-                cDirection = e.keyCode - 36;
-                socket.emit("direction", {'direction' : cDirection});
-                return false;
-            }
-            //right arrow
-            if (e.keyCode == 39) {
-                cDirection = e.keyCode - 36;
-                socket.emit("direction", {'direction' : cDirection});
-                return false;
-            }
-            //down arrow
-            if (e.keyCode == 40) {
-                cDirection = e.keyCode - 36;
-                socket.emit("direction", {'direction' : cDirection});
-                return false;
-            }
+        //1 for grid pixel size
+        var count = 0;
+        for(var i = 0; i < this.size; i+= (squareSize + 1), count += 1) {
+          normal.push(count);
+          squares.push(i + margin.left);
         }
-    });
+        this.nS = normal.length;
 
-socket.on("crash", function(data) {
-        isAlive = false;
-        window.alert("You Lost");
-    });
-/*
-  data =
-  {playberNumber : int,
-  initialDireciton : int,
-  [{'x': xValue, 'y' : yValue}, {'x': xValue, 'y' : yValue}]
-  }
+        this.x = d3.scale.quantize().domain(normal)
+        .range(squares);
 
-*/
-socket.on("start", function(data){
-        isAlive = true;
-        playerNumber = data.playerNumber;
-        cDirection = data.initialDirection;
-        Tron.update(data.initialLocation[1], data.initialLocation[2]);
-    });
+        this.y = d3.scale.quantize().domain(normal)
+        .range(squares);
+      },
+      nSquares: function() {
+        return this.nS || 0;
+      },
+      draw: function() {
+        var svg = this.svg;
+        var rect = svg.append("rect")
+        .attr({
+                x: this.margin.left,
+                y: this.margin.top,
+                width: this.width,
+                height: this.height,
+                fill: 'black'
+            });
+        var grid = svg.append("g").attr("class", "grid");
+        grid.selectAll("line.horizontalGrid").data(this.y.range()).enter()
+        .append("line")
+        .attr(
+  {
+      "class":"horizontalGrid",
+          "x1" : this.x(0),
+          "x2" : this.x(this.size),
+          "y1" : function(d){ return d;},
+          "y2" : function(d){ return d;},
+              "fill" : "none",
+                  "shape-rendering" : "crispEdges",
+                  "stroke" : "green",
+                  "stroke-width" : "1px"
+                  });
 
-socket.on("ready", function(data){
-        if(window.confirm("Are you ready to begin?")){
-            socket.emit("playerready", {});
-        }
-    });
-/*
-  data =
-  { locationList : [{'x': xValue, 'y' : yValue}, {'x': xValue, 'y' : yValue}]
-*/
-socket.on("update", function(data){
-        Tron.update(data.locationList[1], data.locationList[2]);
-    });
+
+          grid.selectAll("line.verticalGrid").data(this.x.range()).enter()
+          .append("line")
+          .attr(
+  {
+      "class":"horizontalGrid",
+          "y1" : this.y(0),
+          "y2" : this.y(this.size),
+          "x1" : function(d){ return d;},
+          "x2" : function(d){ return d;},
+              "fill" : "none",
+                  "shape-rendering" : "crispEdges",
+                  "stroke" : "green",
+                  "stroke-width" : "1px"
+                  });
+      },
+      update: function(player1, player2) {
+        this.players[0].push(player1);
+        this.players[1].push(player2);
+
+        var squares = this.svg.append("g").attr("class", "grid-squares");
+        var p1 = squares.selectAll("rect.square.player1").data(this.players[0]);
+        var p2 = squares.selectAll("rect.square.player2").data(this.players[1]);
+        var x = this.x,
+        y = this.y,
+        squareSize = (this.size/this.gridSize + 1);
+
+        var update_player = function(n, player, color) {
+
+          player.attr("class", "square");
+          player.enter().append("rect")
+          .attr({
+                  class: "square player" + n,
+                      x: function(d) { return x(d.x); },
+                      y: function(d) { return y(d.y); },
+                      width: squareSize,
+                      height: squareSize,
+                      fill: color
+                      });
+        };
+
+        update_player(1, p1, "green");
+        update_player(2, p2, "white");
+      },
+      players: [[],[]],
+      size: 600,
+      gridSize: 50
+    };
+
+    Grid.init();
+    Grid.draw();
+
+    return {
+      update: function(player1, player2) {
+        Grid.update(player1, player2);
+      },
+      size: function() {
+        return Grid.nSquares();
+      }
+    };
+  })();
+
+
+  var cPosition = {'x' : 150, 'y' : 150};
+  //1 - left, 2-up, 3-right, 4-down
+  var cDirection = 2;
+  var MAX_X = 1000;
+  var MAX_Y = 1000;
+  var stepSize = 50;
+
+  var isAlive = false;
+  var playerNumber = 0;
+
+  $(document).keydown(function(e) {
+    if(isAlive){
+      cDirection = (e.keyCode >= 37 || e.keyCode <= 40) ? e.keyCode - 36 : cDirection;
+      console.log("keydown: " + cDirection);
+      socket.emit("direction", { direction: cDirection })
+      return false;
+
+    //TODO: is there a reason to do this? -10 insightful programming
+      //left arrow
+   //   if (e.keyCode == 37) {
+   //     cDirection = e.keyCode - 36;
+   //     socket.emit("direction", {'direction' : cDirection});
+   //     return false;
+   //   }
+   //   //up arrow
+   //   if (e.keyCode == 38) {
+   //     cDirection = e.keyCode - 36;
+   //     socket.emit("direction", {'direction' : cDirection});
+   //     return false;
+   //   }
+   //   //right arrow
+   //   if (e.keyCode == 39) {
+   //     cDirection = e.keyCode - 36;
+   //     socket.emit("direction", {'direction' : cDirection});
+   //     return false;
+   //   }
+   //   //down arrow
+   //   if (e.keyCode == 40) {
+   //     cDirection = e.keyCode - 36;
+   //     socket.emit("direction", {'direction' : cDirection});
+   //     return false;
+   //   }
+    }
+  });
+
+  socket.on("join", function() {
+    window.alert("Welcome to Tron. Use arrow keys to control your hyperbike in the game-realm proper");
+  });
+
+  socket.on("ready", function() {
+    if(window.confirm("Are you ready to begin?")) {
+        socket.emit("playerready", {});
+    }
+  });
+
+  socket.on("start", function(data) {
+    console.log("started");
+    isAlive = true;
+    playerNumber = data.playerNumber;
+    cDirection = data.initialDirection;
+    Tron.update(data.initialLocations[0], data.initialLocations[1]);
+  });
+
+  socket.on("update", function(data) {
+    Tron.update(data.newLocations[0], data.newLocations[1]);
+  });
+
+  socket.on("crash", function() {
+    isAlive = false;
+  });
+
+  socket.on("win", function() {
+    window.alert("You should join the hunger games!");
+  });
+
+  socket.on("lose", function() {
+    window.alert("rekt irl");
+  });
 }
-
-//console.log(Tron.size());
-Tron.update({x: 3, y: 4}, {x: 2, y:2});
-// setTimeout(function() { Tron.update([{x: 3, y: 4}, {x: 4, y: 4}], [{x:10, y:10}]) }, 1000);
