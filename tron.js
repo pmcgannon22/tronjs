2f9bec92e2b5aff9b0c41125a31e86bab3bd93ef
Patrick McGannon <pmcgannon22@gmail.com>
2015-03-25 22:18:07 -0500

    Major refactor

--- tron.js
+++ tron.js
@@ -1,112 +1,155 @@
-var svg = d3.select("#tron").append("svg")
-	.attr("width", 500)
-	.attr("height", 500);
+var Tron = (function() {
+    var Grid = {
+	init: function() {
+	    var margin = {top: 20, right: 10, bottom: 20, left: 20};
+	    this.margin = margin;
+	    this.width = this.size - margin.left - margin.right;
+	    this.height = this.size - margin.top - margin.bottom;
+	    this.svg = d3.select("#tron").append("svg")
+		.attr("width", this.width + margin.left + margin.right)
+		.attr("height", this.height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var rect = svg.append("rect")
-	.attr({
-	    width: 500,
-	    height: 500,
-	    fill: 'black'
-	});
+	    var squareSize = this.size/this.gridSize;
+	    var squares = [];
+	    var normal = [];
+	    
+	    //1 for grid pixel size
+	    var count = 0;
+	    for(var i = 0; i < this.size; i+= (squareSize + 1), count += 1) {
+		normal.push(count);
+		squares.push(i + margin.left);
+	    }
+	    this.nS = normal.length;
+	    console.log(normal);
+	    
+	    this.x = d3.scale.quantize().domain(normal)
+		.range(squares);
+	    
+	    this.y = d3.scale.quantize().domain(normal)
+		.range(squares);
+	},
+	nSquares: function() {
+	    return this.nS || 0;
+	},
+	draw: function() {
+	    var svg = this.svg;
+	    var rect = svg.append("rect")
+		    .attr({
+			x: this.margin.left,
+			y: this.margin.top,
+			width: this.width,
+			height: this.height,
+			fill: 'black'
+		    });
+	    var grid = svg.append("g").attr("class", "grid");
+	    grid.selectAll("line.horizontalGrid").data(this.y.range()).enter()
+		.append("line")
+		.attr(
+		    {
+			"class":"horizontalGrid",
+			"x1" : this.x(0),
+			"x2" : this.x(this.size),
+			"y1" : function(d){ return d;},
+			"y2" : function(d){ return d;},
+			"fill" : "none",
+			"shape-rendering" : "crispEdges",
+			"stroke" : "green",
+			"stroke-width" : "1px"
+		    });
 
-var line = d3.svg.line()
-	.x(function(d) { console.log(d); return d.x; })
-	.y(function(d) { console.log(d); return d.y; })
-	.interpolate("linear");
 
-var data = [ {'x': 75, 'y': 75 }, {'x': 150, 'y': 75}, {'x': 150, 'y': 150} ];
+	    grid.selectAll("line.verticalGrid").data(this.x.range()).enter()
+		.append("line")
+		.attr(
+		    {
+			"class":"horizontalGrid",
+			"y1" : this.y(0),
+			"y2" : this.y(this.size),
+			"x1" : function(d){ return d;},
+			"x2" : function(d){ return d;},
+			"fill" : "none",
+			"shape-rendering" : "crispEdges",
+			"stroke" : "green",
+			"stroke-width" : "1px"
+		    });
+	},
+	update: function(player1, player2) {
+	    this.players[0].push(player1);
+	    this.players[1].push(player2);
+	    
+	    var squares = this.svg.append("g").attr("class", "grid-squares");
+	    var p1 = squares.selectAll("rect.square.player1").data(this.players[0]);
+	    var p2 = squares.selectAll("rect.square.player2").data(this.players[1]);
+	    var x = this.x,
+		y = this.y,
+		squareSize = (this.size/this.gridSize + 1);
+	    
+	    console.log(x);	    
+	    var update_player = function(n, player, color) {
+
+		player.attr("class", "square");
+		player.enter().append("rect")
+		    .attr({
+			class: "square player" + n,
+			x: function(d) { return x(d.x); },
+			y: function(d) { return y(d.y); },
+			width: squareSize,
+    			height: squareSize,
+			fill: color
+		    });
+	    };
+
+	    update_player(1, p1, "green");
+	    update_player(2, p2, "white");
+	},
+	players: [[],[]],
+	size: 600,
+	gridSize: 50
+    };
+
+    Grid.init();
+    Grid.draw();
+    //Grid.update([{x: 1, y:1}], [{x:5, y:10}]);
 
 
-function update(data) {
-    var circle = svg.selectAll("circle").data(data);
 
-    circle.enter().append("circle")
-	.attr("cx", function(d) { return d.x; })
-	.attr("cy", function(d) { return d.y; } )
-	.attr("r", 10)
-	.attr("fill", "steelblue");
-}
-//circle.exit().remove();
-/*var paths = svg.selectAll("path").data(data);
+    
 
-    paths.enter().append("path")
-	.attr("class", "tron-path")
-	.attr("d", line(data))
-	.attr("stroke", "blue")
-	.attr("stroke-width", 2)
-	.attr("fill", "none");
-
-paths.exit().remove();*/
-
-
-setInterval(function() {
-    data.push({'x': data[data.length - 1].x, 'y': data[data.length - 1].y + 50});
-    update(data);
-}, 1000);
+    /*var c = 0;
+     setInterval(function() {
+     c++;
+     data.push({'x': c, 'y': 0});
+     update(data);
+     }, 1000);*/
 
 
 
 
-	
+    /*var KeyboardListener = {
+	init: function(config) {
+	    this
+	 onKeyDown: function(callback) {
+	     $(document).keydown(function(e) {
+		 callback(e);
+	     });
+	 }
+     }; */
+	 
+     
 
-var cPosition = {'x' : 150, 'y' : 150};
-//1 - left, 2-up, 3-right, 4-down 
-var cDirection = 2;
-var MAX_X = 1000;
-var MAX_Y = 1000;
-var stepSize = 50;
+    return {
+	update: function(player1, player2) {
+	    Grid.update(player1, player2);
+	},
+	size: function() {
+	    return Grid.nSquares();
+	}
+    };
+})();
 
-$(document).keydown(function(e){
-        //left arrow
-        if (e.keyCode == 37) {
-            cDirection = e.keyCode - 36;
-            stepLocation();
-            return false;
-        }
-        //up arrow
-        if (e.keyCode == 38) {
-            cDirection = e.keyCode - 36;
-            stepLocation();
-            return false;
-        }
-        //right arrow
-        if (e.keyCode == 39) {
-            cDirection = e.keyCode - 36;
-            stepLocation();
-            return false;
-        }
-        //down arrow
-        if (e.keyCode == 40) {
-            cDirection = e.keyCode - 36;
-            stepLocation();
-            return false;
-        }
-});
 
-function stepLocation(){
-    var tempPosition = cPosition;
-    switch (cDirection){
-    case 1:
-        tempPosition.x -= stepSize;
-        break;
-    case 2:
-        tempPosition.y += stepSize;
-        break;
-    case 3:
-        tempPosition.x += stepSize;
-        break;
-    case 4:
-        tempPosition.y -= stepSize;
-        break;   
-    }
-    if(tempPosition.x < 0 || tempPosition.y < 0 ||
-       tempPosition.x > MAX_X ||
-       tempPosition.y > MAX_Y)
-        {
-            window.alert ("You died");
-        }
-    else {
-        cPosition = tempPosition;
-        p.push(cPosition);
-    }
-}
+console.log(Tron.size());
+Tron.update({x: 3, y: 4}, {x: 2, y:2});
+//setTimeout(function() { Tron.update([{x: 3, y: 4}, {x: 4, y: 4}], [{x:10, y:10}]) }, 1000);
